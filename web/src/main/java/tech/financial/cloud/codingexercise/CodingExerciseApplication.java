3310c1ad9b3d3e23ba369eb79236e9a8ce6da69c
Anatolie Lupacescu <anatollupacescu@gmail.com>
2017-12-06 11:31:13 +0100

    initial commit: skeleton only

---
+++ web/src/main/java/tech/financial/cloud/codingexercise/CodingExerciseApplication.java
@@ -0,0 +1,12 @@
+package tech.financial.cloud.codingexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingExerciseApplication.class, args);
+	}
+}
